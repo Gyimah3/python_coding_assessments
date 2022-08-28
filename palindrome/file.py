a6a4c4e2cd95c171cc1344d34a0481853b4301a3
Gyimah Gideon <gideongyimah19@gmail.com>
2022-08-28 22:48:30 +0000

    Update file.py

--- palindrome/file.py
+++ palindrome/file.py
@@ -1,32 +1,27 @@
 # This function must contain your implementation of the solution
-def solution(input, **kwargs):
-    """__docstring__
+def solution(input1, **kwargs):
+    reversed_input1 = input1[::-1]
+    status=1
+    if(input1!=reversed_input1):
+        status=0
+    return status
 
-    Parameters
-    ----------
-    variable : type(variable)
-        Description of parameter `variable`.
 
-    Returns
-    -------
-    variable : type(variable)
-        Description of parameter `variable`, different possible value.
-    """
-    #
-    # @TODO: Implement it
-    #
-    return
+    
+   
+   
 
 
 # The main function where you must built the user-interface as
 # a command-line interface for everyone to use the made function
 # using this interface
 def main():
-    """__docstring__"""
-    #
-    # @TODO: Implement it
-    #
-
+   input1 = input("Enter the input1: ")
+status= check_palindrome_1(input1)
+if(status):
+    print("It is a palindrome ")
+else:
+    print("Sorry! Try again")
 
 if __name__ == "__main__":
     main()
