6b79f433336aa8f3920ce6dbaec217688fa947ae
Gyimah Gideon <gideongyimah19@gmail.com>
2022-08-28 23:48:06 +0000

    Update file.py

--- file_owners/file.py
+++ file_owners/file.py
@@ -1,32 +1,17 @@
 # This function must contain your implementation of the solution
-def solution(input_dictionary, **kwargs):
-    """__docstring__
-
-    Parameters
-    ----------
-    variable : type(variable)
-        Description of parameter `variable`.
-
-    Returns
-    -------
-    variable : type(variable)
-        Description of parameter `variable`, different possible value.
-    """
-    #
-    # @TODO: Implement it
-    #
-    return
+def solution(files, **kwargs):
+    owners = defaultdict(list)
+    for file, owner in files.items():
+        owners[owner].append(file)
+    return 
 
 
 # The main function where you must built the user-interface as
 # a command-line interface for everyone to use the made function
 # using this interface
 def main():
-    """__docstring__"""
-    #
-    # @TODO: Implement it
-    #
-
+    files = {'Input.txt': 'Randy','Code.py': 'Stan','Output.txt': 'Randy'}
+ return solution
 
 if __name__ == "__main__":
     main()
